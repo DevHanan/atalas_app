f285ace2a455f6e4ecc6ab9a5483f40b1cea8392
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2023-12-04 21:03:40 +0200

    rearrange sidebar

--- resources/views/admin/layouts/inc/sidebar.blade.php
+++ resources/views/admin/layouts/inc/sidebar.blade.php
@@ -5,14 +5,14 @@
         <li class="nav-item {{ Request::is('admin/dashboard*') ? 'active' : '' }}">
             <a href="{{ route('admin.dashboard.index') }}" class="nav-link">
                 <span class="pcoded-micon"><i class="fas fa-desktop"></i></span>
-                <span class="pcoded-mtext"> لوحة التحكم</span>
+                <span class="pcoded-mtext">  الرئيسية</span>
             </a>
         </li>
-
+      
         <li class="nav-item pcoded-hasmenu @if ( Request::is('admin/province*')  || Request::is('admin/district*')) pcoded-trigger @endif ">
             <a href="#!" class="nav-link">
                 <span class="pcoded-micon"><i class="fa fa-info-circle" aria-hidden="true"></i></span>
-                <span class="pcoded-mtext">البيانات الأساسية</span>
+                <span class="pcoded-mtext"> إعدادات النظام</span>
             </a>
             <ul class="pcoded-submenu" @if ( Request::is('admin/province') || Request::is('admin/district') || Request::is('admin/sliders') || Request::is('admin/pages')) style="display:block;" @else style="display:none;" @endif>
 
@@ -37,26 +37,6 @@
 
             </ul>
         </li>
-        <li class="{{ Request::is('admn/clients*') ? 'active' : '' }}">
-            <a href="{{ route('admin.clients.index') }}" class="">
-                <span class="pcoded-micon"><i class="fa fa-user"></i></span>
-                <span class="pcoded-mtext">
-                    العملاء </span></a>
-        </li>
-      
-        <li class="{{ Request::is('admn/orders*') ? 'active' : '' }}">
-            <a href="{{ route('admin.orders.index') }}" class="">
-                <span class="pcoded-micon"><i class="fa fa-first-order"></i></span>
-                <span class="pcoded-mtext">
-                    الطلبات </span></a>
-        </li>
-        <li class="{{ Request::is('admn/visits*') ? 'active' : '' }}">
-            <a href="{{ route('admin.visits.index') }}" class="">
-                <span class="pcoded-micon"><i class="fa fa-eye"></i></span>
-                <span class="pcoded-mtext">
-                    الزيارات </span></a>
-        </li>
-
         <li class="nav-item pcoded-hasmenu @if ( Request::is('admin/sales*')  ) pcoded-trigger @endif ">
             <a href="#!" class="nav-link">
                 <span class="pcoded-micon"><i class="fas fa-users-cog"></i></span>
@@ -91,8 +71,6 @@
 
             </ul>
         </li>
-
-
         <li class="nav-item pcoded-hasmenu @if ( Request::is('admin/sales*') ) pcoded-trigger @endif ">
             <a href="#!" class="nav-link">
                 <span class="pcoded-micon"><i class="fas fa-users-cog"></i></span>
@@ -117,6 +95,30 @@
 
             </ul>
         </li>
+        <li class="{{ Request::is('admn/clients*') ? 'active' : '' }}">
+            <a href="{{ route('admin.clients.index') }}" class="">
+                <span class="pcoded-micon"><i class="fa fa-user"></i></span>
+                <span class="pcoded-mtext">
+                    العملاء </span></a>
+        </li>
+      
+        <li class="{{ Request::is('admn/orders*') ? 'active' : '' }}">
+            <a href="{{ route('admin.orders.index') }}" class="">
+                <span class="pcoded-micon"><i class="fa fa-first-order"></i></span>
+                <span class="pcoded-mtext">
+                    الطلبات </span></a>
+        </li>
+        <li class="{{ Request::is('admn/visits*') ? 'active' : '' }}">
+            <a href="{{ route('admin.visits.index') }}" class="">
+                <span class="pcoded-micon"><i class="fa fa-eye"></i></span>
+                <span class="pcoded-mtext">
+                    الزيارات </span></a>
+        </li>
+
+        
+
+
+     
 <!-- 
         <li class="{{ Request::is('admn/users/staff*') ? 'active' : '' }}">
             <a href="{{ url('admn/users/staff') }}" class="">
@@ -135,7 +137,7 @@
         <li class="{{ Request::is('admin/setting') ? 'active' : '' }}"><a href="{{ route('admin.setting.index') }}" class="">
                 <span class="pcoded-micon"><i class="fas fa-user-edit"></i></span>
                 <span class="pcoded-mtext">
-                    الإعدادت العامة </span> </a></li>
+                     إعدادت النظام </span> </a></li>
 
 
 
