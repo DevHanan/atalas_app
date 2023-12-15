1e7cc9e34c5c506117150fcec441fb4c0823fb50
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2023-12-15 22:35:24 +0200

    add filter status

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -26,6 +26,6 @@ class Order extends Model
     
      public function delivery(){
       
-        return $this->belongsTo(Sale::class);
+        return $this->belongsTo(Sale::class,'sale_id','id');
     }
 }
