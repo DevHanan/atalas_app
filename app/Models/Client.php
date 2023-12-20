985bf66d112f858252c83ef4b0ffd1dd91c3e3e8
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2023-12-20 21:39:55 +0200

    update

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -11,6 +11,7 @@ class Client extends Authenticatable
 {
     use HasFactory, HasApiTokens;
     protected $fillable = ['first_name','last_name','lat','lng','phone','email','province_id','district_id','password','sale_id'];
+    protected $hidden =['api_token','password'];
     protected $guard='clients';
 
     public function district(){
