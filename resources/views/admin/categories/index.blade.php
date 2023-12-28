58cb105d6d30a6187af6620796b6e079a1af4e6f
hanan el-metwally taha ahmed <hanan.taha1993@gmail.com>
2023-12-29 01:11:23 +0200

    check debug

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -31,29 +31,29 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                 @foreach($categories as $category)
+                                 @foreach($categories as $row)
 					<tr >
-						<td class="ui-state-default drag-handler" data-faq="{{$category->id}}">{{$category->id}}</td>
-						<td>{{$category->title}}</td>
+						<td class="ui-state-default drag-handler" data-faq="{{$row->id}}">{{$row->id}}</td>
+						<td>{{$row->title}}</td>
                         <td>
-                                            @if( $category->status == 1 )
+                                            @if( $row->status == 1 )
                                             <span class="badge badge-pill badge-success">{{ __('status_active') }}</span>
                                             @else
                                             <span class="badge badge-pill badge-danger">{{ __('status_inactive') }}</span>
                                             @endif
                                         </td>
-						<td> {{ optional($category->section)->title }} </td>
-<td><img src="{{asset($category->image)}}" style="width:40px"></td>					
+						<td> {{ optional($row->section)->title }} </td>
+<td><img src="{{asset($row->image)}}" style="width:40px"></td>					
 						<td style="width: 270px;">
 
 					 
 
-							<a href="{{url('admin/categories/'.$category->id.'/edit')}}">
+							<a href="{{url('admin/categories/'.$row->id.'/edit')}}">
 								<span class="btn  btn-outline-success btn-sm font-1 mx-1">
 									<span class="fas fa-wrench "></span> {{__('modal_edit')}}
 								</span>
 							</a>
-                            <button type="button" class="btn btn-icon btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal-{{ $category->id }}">
+                            <button type="button" class="btn btn-icon btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal-{{ $row->id }}">
                                                 <i class="fas fa-trash-alt"></i>
                                             </button>
                                             <!-- Include Delete modal -->
